321bbcf0f5489ea370c4473461388f49c6b8ee7f
jan <janfrancistagadiad@gmail.com>
2016-01-06 19:49:06 +0800

    [update] renaming classes

--- app/src/main/java/com/example/android/buktogo/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/android/buktogo/Fragments/HomeFragment.java
@@ -14,7 +14,7 @@ import com.example.android.buktogo.R;
 public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_rue, container, false);
         return rootView;
     }
 }
